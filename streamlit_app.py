afb1c07e2ae7aa7cfd2bd9567a20431358f73597
Angel Samuel Parra <113923852+angelparra1983@users.noreply.github.com>
2023-04-01 15:42:17 -0400

    Update streamlit_app.py
    Add a STOP Command,

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -35,6 +36,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put the data in a dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+# don't run anathing past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
